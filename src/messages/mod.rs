55a89f229bcea8c23eba30e105f2c749a3a4bee7
Alaric <alaric@arcanazero.com>
2022-05-22 23:13:39 -0600

    Add system alert message.

--- src/messages/mod.rs
+++ src/messages/mod.rs
@@ -1,10 +1,15 @@
 use crate::prelude::*;
 
+mod system_alerts;
+
+pub use system_alerts::*;
+
 pub struct MessagePlugin;
 
 impl Plugin for MessagePlugin {
     fn build(&self, app: &mut App) {
-        app.add_system_to_stage(CoreStage::Last, clear_handled_messages);
+        app.add_system_to_stage(CoreStage::Last, clear_handled_messages)
+            .add_system(display_system_messages);
     }
 }
 
@@ -20,18 +25,8 @@ pub struct Target(pub Entity);
 #[derive(Component)]
 pub struct Sender(pub Entity);
 
-#[derive(Component)]
-pub struct SystemAlert(pub String);
-
 fn clear_handled_messages(query: Query<(Entity, &Message, &IsHandled)>, mut commands: Commands) {
     query.iter().for_each(|(entity, _, _)| {
         commands.entity(entity).despawn_recursive();
     });
-}
-
-pub fn create_system_alert_message(commands: &mut Commands, message: &str) {
-    commands
-        .spawn()
-        .insert(Message)
-        .insert(SystemAlert(message.to_string()));
 }
